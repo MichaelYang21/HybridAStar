0a65095a5e067cd33ff532a423e91e25efd42314
Michael Yang <zzyang21@gmail.com>
2021-11-27 23:51:52 -0500

    Added Stanford junior as the reference paper;

--- build-Debug/HybridAStar/up_up_up_Users_Henry_Documents_MyWorkspace_TrajectoryGeneration_HybridAstart_HybridAstart_hybrid_breadth_first.cpp.o.d
+++
@@ -1,5 +0,0 @@
-../build-Debug/HybridAStar/up_up_up_Users_Henry_Documents_MyWorkspace_TrajectoryGeneration_HybridAstart_HybridAstart_hybrid_breadth_first.cpp.o: \
- C:/Users/Henry/Documents/MyWorkspace/TrajectoryGeneration/HybridAstart/HybridAstart/hybrid_breadth_first.cpp \
- C:/Users/Henry/Documents/MyWorkspace/TrajectoryGeneration/HybridAstart/HybridAstart/hybrid_breadth_first.h
-
-C:/Users/Henry/Documents/MyWorkspace/TrajectoryGeneration/HybridAstart/HybridAstart/hybrid_breadth_first.h:
